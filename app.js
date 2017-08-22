d8518f3688b4c35d705db53e014faa6a982ed538
Jason Merrill <jwmerrill@gmail.com>
2017-08-21 17:43:49 -0700

    Specify total files instead of tree depth.

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ const app = express()
 
 app.use(express.static(__dirname));
 
+function generalParseInt(digits, radix) {
+  // Apparently, parseInt is limited to radix 2-36
+  var total = 0;
+  for (var i = 0; i < digits.length; i++) {
+    total *= radix;
+    total += parseInt(digits[i], 10);
+  }
+  return total;
+}
+
+function index(branchingFactor, digits) {
+  var depth = digits.length - 1; // Expect leading digit to be 0
+  if (depth === 0) return 1;
+  if (branchingFactor === 1) return depth + 1;
+  var offset = 1 + (Math.pow(branchingFactor, depth) - 1) / (branchingFactor - 1);
+  return offset + generalParseInt(digits, branchingFactor);
+}
+
 app.get(/^\/tree\/(\d+)\/(\d+)\/(\d+)/, function (req, res) {
   var match = req.path.match(/^\/tree((\/\d+)+)/)
 
   var pieces = match[1].split('/');
-  var depth = parseInt(pieces[1], 10);
+  var numFiles = parseInt(pieces[1], 10);
   var branchingFactor = parseInt(pieces[2], 10);
-  var currentDepth = pieces.length - 4;
+  var digits = pieces.slice(3);
 
   var out;
-  if (currentDepth >= depth) {
+  if (index(branchingFactor, digits) > numFiles) {
     out = "";
   } else {
     var outPieces = [];
     outPieces.push("define(function(require) {");
-    for (var i = 0; i < branchingFactor; i++) {
+    for (var i = 0; i < branchingFactor && index(branchingFactor, digits.concat(i)) <= numFiles; i++) {
       outPieces.push("  require('tree" + match[1] + '/' + i + "');");
     }
     outPieces.push("});");
